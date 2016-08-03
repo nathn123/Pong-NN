4873591b89522ae5fc565b29f652b9dd2911b633
nathn123 <nathaneil123@hotmail.co.uk>
2016-08-03 22:21:38 +0100

    Neuron Designs, Starting UI
    Designed Neuron and the Net, Started UI elements need scripts to link
    score, ui and net learning

---
+++ Assets/AiController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class AiController : MonoBehaviour {
+
+	// Use this for initialization
+    Neuron Output, BallPosX,BallPosY,BallVelX, BallVelY;
+    List<Neuron> HiddenLayer;
+    int hiddenlayersize, saveinteval;
+    bool save;
+
+    
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void Gen()
+    {
+        //due to constructor of the neuron, we create the network back to front
+        Output = new Neuron("Output",new List<float>(),new List<Neuron>());
+        HiddenLayer = new List<Neuron>();
+        var OutputList = new List<Neuron>();
+        var WeightList = new List<float>();
+        OutputList.Add(Output);
+        for(int i = 0; i < hiddenlayersize;++i)
+        {
+            WeightList.Add(Random.Range(-1.0f,1.0f));
+            HiddenLayer.Add(new Neuron("HiddenNeuron" + i.ToString(), WeightList, OutputList));
+            WeightList.Clear(); // only one weight needed
+        }
+
+        for (int i = 0; i < HiddenLayer.Count; ++i)
+        {
+            WeightList.Add(Random.Range(-1.0f, 1.0f));
+        }
+        BallPosX = new Neuron("BallPosX", WeightList, HiddenLayer);
+        WeightList.Clear();
+
+        for (int i = 0; i < HiddenLayer.Count; ++i)
+        {
+            WeightList.Add(Random.Range(-1.0f, 1.0f));
+        }
+        BallPosY = new Neuron("BallPosX", WeightList, HiddenLayer);
+        WeightList.Clear();
+
+        for (int i = 0; i < HiddenLayer.Count; ++i)
+        {
+            WeightList.Add(Random.Range(-1.0f, 1.0f));
+        }
+        BallVelX = new Neuron("BallPosX", WeightList, HiddenLayer);
+        WeightList.Clear();
+
+        for (int i = 0; i < HiddenLayer.Count; ++i)
+        {
+            WeightList.Add(Random.Range(-1.0f, 1.0f));
+        }
+        BallVelY = new Neuron("BallPosX", WeightList, HiddenLayer);
+        WeightList.Clear();
+
+    }
+
+    public void Save()
+    {
+
+    }
+    public void Load()
+    {
+
+    }
+}
---
+++ Assets/Neuron.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+public class Neuron {
+
+	// Use this for initialization
+    float input, output;
+    List<float> weights;
+    List<Neuron> Outputs;
+
+    [XmlAttribute("name")]
+    string Name;
+
+    public Neuron(string Name_,List<float> weights_, List<Neuron> Outputs_)
+    {
+        Name = Name_;
+        weights = weights_;
+        Outputs = Outputs_;
+    }
+    public void Input(float inputval)
+    {
+
+    }
+
+    void Output()
+    {
+        if(EvaluationFunction() > 0)
+        {
+            for(int i = 0; i < Outputs.Count; i++)
+            {
+                Outputs[i].Input(1 * weights[i]);
+            }
+        }
+
+    }
+    float EvaluationFunction()
+    {
+        return 0;
+    }
+    void Save()
+    {
+
+    }
+}
